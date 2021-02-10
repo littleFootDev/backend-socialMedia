5cb453e89959fda8c8f12b8555ac3e1bc6790779
LittleFootDev <thelittlefootdev@gmail.com>
2021-02-10 22:14:03 +0100

    CRUD fonctionnel

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -3,7 +3,10 @@ import  Post  from "../models/Post";
 const postRouter = Router();
 
 postRouter.get('/', (req, res) => {
-    res.send("tous les posts");
+    Post.find({}, (err, datas) => {
+        if (err) console.error(err);
+        res.send(datas);
+    })
 })
 
 postRouter.post("/new", (req, res) => {
@@ -13,5 +16,30 @@ postRouter.post("/new", (req, res) => {
         res.send(data)
     });
 })
+
+postRouter.get("/:post_id", (req, res) => {
+   Post.findById( {_id:req.params.post_id}, (err, data) => {
+        if (err) console.error(err);
+        res.send(data);
+   })
+})
+postRouter.put("/:post_id", (req, res) => {
+   Post.findById( {_id:req.params.post_id}, (err, data) => {
+        if (err) console.error(err);
+        Object.assign(data, req.body ).save((err, data) => {
+            if (err) console.error(err);
+            res.send(data);
+        })
+   })
+})
+
+postRouter.delete("/:post_id", (req, res) => {
+    Post.remove({_id:req.params.post_id}, (err) => {
+        if (err) console.error(err);
+        res.send("le post à bien été suprimé !");
+    })
+})
+
+
 
 export default postRouter;
